f49d1cb49f2eb1fd9829d1e5e5c4c28b295bfb9e
Minh Monmen <minhpq331@gmail.com>
2022-03-22 11:42:24 +0700

    update timer

--- src/common/user/user.event.ts
+++ src/common/user/user.event.ts
@@ -3,6 +3,10 @@ import logger from '@common/logger';
 import { ISample } from '@common/sample/Sample';
 import { EVENT_SAMPLE_CREATED } from '@common/sample/sample.event';
 
+function timeout(ms: number): Promise<void> {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
 export class UserEvent {
     public static register(): void {
         // register handler
@@ -11,5 +15,7 @@ export class UserEvent {
 
     private static async SampleCreatedHandler(sample: ISample): Promise<void> {
         logger.info(`Process increase sample counter for user: ${sample._id.toHexString()}`);
+        await timeout(3000);
+        logger.info(`Delay not affect api or worker processing`);
     }
 }
